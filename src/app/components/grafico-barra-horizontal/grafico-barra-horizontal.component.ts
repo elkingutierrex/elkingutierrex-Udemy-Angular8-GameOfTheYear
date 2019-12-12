7203b78a4f6510e4dca602a16ea62c99d2f60f61
elkingutierrex <Pro-egutierrez@intergrupo.com>
2019-12-12 08:13:07 -0500

    330 - Cambiar valores de la gráfica de forma aleatoria

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent {
+export class GraficoBarraHorizontalComponent implements OnDestroy{
 
   results: any[] = [{
     "name": "Juego1",
@@ -37,14 +37,34 @@ export class GraficoBarraHorizontalComponent {
   showYAxisLabel = true;
   yAxisLabel = 'Votos';
 
-  colorScheme = 'nightLights'
+  colorScheme = 'nightLights';
+
+  intervalo;
 
   constructor() {
+
+    this.intervalo = setInterval( ()=>  {
+      console.log("tick");
+
+      const newResults = [...this.results];
+
+      for (let i in newResults){
+        this.results[i].value = Math.round(Math.random()* 500);
+      }
+      this.results = [...newResults];
+  
+    },1500)
+    
+
  
   }
 
   onSelect(event) {
     console.log(event);
+  }
+
+  ngOnDestroy(){
+    clearInterval( this.intervalo );
   }
 
 }
